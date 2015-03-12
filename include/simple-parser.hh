405ddaf71329ba0db7f04e7a43d5976468e00088
David Bjergaard <dbjergaard@gmail.com>
2015-03-12 13:39:11 -0400

    Cut class hacks

--- include/simple-parser.hh
+++ include/simple-parser.hh
@@ -9,3 +9,5 @@ enum ERROR{KEY_PARSE_ERROR, VALUE_PARSE_ERROR, FILE_IO_ERROR};
 int parse_file(std::ifstream& file,std::vector<std::vector<std::string > >& options);
 void get_opts(const char* opt_fname,std::map<std::string,std::string>& value_opts,
 	      real_cuts& CutDefReals, category_cuts& CutDefCats);
+void get_opts(const char* opt_fname,std::map<std::string,std::string>& value_opts,
+	      cut_container& cuts);
