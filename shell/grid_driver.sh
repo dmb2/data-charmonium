e696f3ef2ffcea79a3b4b684cfd59a56a21da2a3
David Bjergaard <dbjergaard@gmail.com>
2015-11-19 13:37:05 -0500

    Grid driver tweaks

--- shell/grid_driver.sh
+++ shell/grid_driver.sh
@@ -1,9 +1,9 @@
-local INFILES=$(echo "$1" | tr ',' ' ')
-local CONFIG=$2
-local XS=$3
-local result_files=""
-count = 0
-for f in "$INFILES"
+INFILES=$(echo "$1" | tr ',' ' ')
+CONFIG=$2
+XS=$3
+result_files=""
+count=0
+for f in $INFILES
 do
     out_file=$(basename "$f")
     out_file="${count}${out_file%.*}.mini.root"
@@ -15,9 +15,11 @@ done
 if [ "$CONFIG" == "Systematics.conf" ]; then
     for syst in MuonSmeared{IDUp,MSUp,Low,Up,} TrackZ{Filtered,Smeared}Jets
     do
+	echo hadd "total_${syst}.mini.root" *${syst}.mini.root
 	hadd "total_${syst}.mini.root" *${syst}.mini.root
     done
 else
+    echo hadd output.mini.root "$result_files"
     hadd output.mini.root "$result_files"
 fi
    
