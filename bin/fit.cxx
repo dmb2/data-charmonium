ac8b0235bd44a85c6a1e9d2cbb682579a145b640
David Bjergaard <dbjergaard@gmail.com>
2016-04-13 16:27:17 -0400

    projected out sbs and fit utilities

--- bin/fit.cxx
+++ bin/fit.cxx
@@ -3,12 +3,8 @@
 #include "histo-meta-data.hh"
 #include "fit-utils.hh"
 #include "histo-utils.hh"
-#include "sbs-utils.hh"
 
 #include "TFile.h"
-#include "TLegend.h"
-#include "TCanvas.h"
-#include "TH1D.h"
 #include "TTree.h"
 
 #include "RooRealVar.h"
@@ -17,15 +13,18 @@
 #include "RooDataSet.h"
 
 void usage(const char* name){
-  MSG("Usage: "<<name<<" -i input.root -t tree_name -l lumi");
+  MSG("Usage: "<<name<<" -i input.root -t tree_name -l lumi -o fitresult.root");
 }
 
 void jpsi_fit(TTree* tree, RooRealVar* mass, RooRealVar* tau,
-	      std::map<std::string,sb_info>& sep_var_info, const double lumi){
+	      /*std::map<std::string,sb_info>& sep_var_info,*/ const double lumi){
   RooDataSet data("data","data",RooArgSet(*mass,*tau),RooFit::Import(*tree));
   RooAbsPdf* model = build_model(mass,tau);
   RooFitResult* result = Fit(model,data);
+  result->SetName("result");
   result->Print();
+  result->Write();
+  /*
   print_plot(mass,&data,model,"mass",";J/#psi Mass [GeV]",lumi);
   print_plot(tau,&data,model,"tau",";J/#psi Proper Decay Time [ps]",lumi);
 
@@ -61,16 +60,17 @@ void jpsi_fit(TTree* tree, RooRealVar* mass, RooRealVar* tau,
   sep_var_info["tau"].regions=tau->getBinningNames();
   sep_var_info["tau"].sts_ratio=div(get_yield(tau_sig,tau,"Sig",covmat),
 				    get_yield(tau_sig,tau,"SB",covmat));
-
+  */
 
 }
 int main(const int argc, char* const argv[]){
   char* inFName=NULL;
+  char* outFName=NULL;
   char* tree_name = NULL;
   int c;
   double lumi;
   
-  while((c = getopt(argc,argv,"i:l:t:"))!= -1){
+  while((c = getopt(argc,argv,"i:l:t:o:"))!= -1){
     switch(c){
     case 'i':
       inFName=optarg;
@@ -81,6 +81,9 @@ int main(const int argc, char* const argv[]){
     case 't':
       tree_name=optarg;
       break;
+    case 'o':
+      outFName=optarg;
+      break;
     default:
       abort();
     }
@@ -89,18 +92,27 @@ int main(const int argc, char* const argv[]){
     usage(argv[0]);
     exit(1);
   }
+  if(outFName==NULL){
+    //waste not want not
+    outFName=(char*)calloc(16,sizeof(char));
+    strncpy(outFName,"fitresult.root",16);
+  }
   
   setup_global_style();
 
   TFile* file = TFile::Open(inFName);
   TTree* tree = retrieve<TTree>(file,tree_name);
-
-
+  MSG_DEBUG("Fit result is stored in: "<<outFName);
   
   RooRealVar *mass = new RooRealVar("jpsi_m","jpsi_m",JPSIMASS, JPSIMASS-0.4, JPSIMASS+0.5); // stay away from the psi(2S)
   RooRealVar *tau = new RooRealVar("jpsi_tau","Lifetime",-2.,5);
-  std::map<std::string,sb_info> sep_var_info;
-  jpsi_fit(tree,mass,tau,sep_var_info,lumi);
+  // std::map<std::string,sb_info> sep_var_info;
+  //,sep_var_info
+  TFile out_file(outFName,"RECREATE");
+  jpsi_fit(tree,mass,tau,lumi);
+  out_file.Write();
+  out_file.Close();
+  /*
   MSG_DEBUG(make_cut_expr(mass->getBinningNames(),"Sig") + " && "
 	    + make_cut_expr(tau->getBinningNames(),"Sig"));
   for(std::map<std::string,sb_info>::const_iterator it=sep_var_info.begin(); it !=sep_var_info.end(); ++it){
@@ -130,6 +142,6 @@ int main(const int argc, char* const argv[]){
 				     sep_var_info,lumi);
     print_pythia_stack(HistBook[variables[i]],sig_final,lumi,cut_expr,".pdf");
   }
-
+  */
   return 0;
 }
