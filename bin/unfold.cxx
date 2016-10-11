449567752ae6a6d502a4c1cac94c2f002767a819
David Bjergaard <dbjergaard@gmail.com>
2016-10-11 14:37:40 -0400

    Snapshot

--- bin/unfold.cxx
+++ bin/unfold.cxx
@@ -1,5 +1,6 @@
 
 #include "TLegend.h"
+#include "TLatex.h"
 #include "TTree.h"
 #include "TKey.h"
 #include "TCanvas.h"
@@ -16,17 +17,17 @@
 #include "RooUnfoldSvd.h"
 
 void usage(const char* prog_name){
-  MSG("Usage: "<<prog_name<< "-t truth_file.root -i in_file.root -o out_name");
+  MSG("Usage: "<<prog_name<< " -t truth_file.root -i in_file.root -n N -v");
 }
 
 int main(const int argc, char* const argv[]){
   char* in_fname=NULL;
   char* truth_fname=NULL;
-  char* out_fname=NULL;
+  // char* out_fname=NULL;
   int num_iter=4;
   bool print_validation_plots=false;
   int c;
-  while((c = getopt(argc,argv,"vn:t:i:o:")) != -1){
+  while((c = getopt(argc,argv,"vn:t:i:")) != -1){
     switch(c){
     case 'v':
       print_validation_plots=true;
@@ -40,9 +41,9 @@ int main(const int argc, char* const argv[]){
     case 'i':
       in_fname = optarg;
       break;
-    case 'o':
-      out_fname = optarg;
-      break;
+    // case 'o':
+    //   out_fname = optarg;
+    //   break;
     default:
       abort();
     }
@@ -57,91 +58,96 @@ int main(const int argc, char* const argv[]){
   // heat_gradient(gStyle,stops,LEN(stops));
   MSG("Input File: "<<in_fname);
   MSG("Truth File: "<<truth_fname);
-  MSG("Output File: "<<out_fname);
   TTree* tree = retrieve<TTree>(truth_fname,"mini");
   TFile* reco_file = TFile::Open(in_fname);
   TIter iter(reco_file->GetListOfKeys());
   TKey *key=NULL;
   while((key=(TKey*)iter())){
-    if(std::string(key->GetClassName()).find("TH")!=std::string::npos){
-      TH1* hist = dynamic_cast<TH1*>(key->ReadObj());
-      if(hist==nullptr){
-	MSG_ERR("Error casting hist of type: "<<key->ClassName()<<" to TH1*, object named "<<key->GetName());
-      }
-      const std::string name(hist->GetName());
-      TH1* base_hist = dynamic_cast<TH1*>(hist->Clone(name.c_str()));
-      for(int i =0; i < base_hist->GetNbinsX(); i++){
-	base_hist->SetBinContent(i,0);
-	base_hist->SetBinError(i,0);
-      }
-      TH2D* response_hist = setup_response_hist(base_hist);
-      response_hist = dynamic_cast<TH2D*>(make_response_hist(response_hist,tree,base_hist->GetName(),"SF*weight","_response"));
-      TH1* reco_hist = make_normal_hist(base_hist,tree,base_hist->GetName(),"SF*weight","_reco");
-      TH1* truth_hist = make_normal_hist(base_hist,tree,(std::string("truth_")+base_hist->GetName()),"SF*weight","_truth");
-      double N_sig = hist->Integral();
-      double N_MC = reco_hist->Integral();
-      MSG_DEBUG("NSig: "<<N_sig<<" N_MC: "<<N_MC);
-      reco_hist->Scale(N_sig/N_MC);
-      truth_hist->Scale(N_sig/N_MC);//n_truth?
-      if(print_validation_plots){
-	//TODO Polish these and put them in supplement
-	TCanvas debug("debug","debug",600,600);
-	TLegend* leg = init_legend();
-	leg->AddEntry(reco_hist,"MC Reco","l");
-	leg->AddEntry(truth_hist,"MC Truth","l");
-	leg->AddEntry(hist,"Reco","l");
-	reco_hist->SetLineColor(kBlue);
-	reco_hist->Draw("HIST");
-	truth_hist->SetLineColor(kRed);
-	truth_hist->Draw("HIST same");
-	hist->SetLineColor(kBlack);
-	hist->Draw("HIST same");
-	leg->Draw();
-	debug.SaveAs((name+"reco_truth_debug.pdf").c_str());
-	debug.Clear();
-	response_hist->Draw("COLZ");
-	debug.SaveAs((name+"response_debug.pdf").c_str());
-      }
-      TH1D* unfolded=NULL;
-      RooUnfoldResponse response(reco_hist, truth_hist,response_hist,
-				 (name+"_unfolded").c_str(),hist->GetTitle());
-      // RooUnfoldBinByBin UnfoldBBB(&response,hist);
-      RooUnfoldBayes unfold(&response,hist,num_iter);
-      // unfold.SetSmoothing(true);
+    const std::string name(key->GetName());
+    if(std::string(key->GetClassName()).find("TH")==std::string::npos ||
+       !(name=="delta_r" || name=="jet_z")){
+      continue;
+    }
+    MSG_DEBUG(name);
+    TH1* hist = dynamic_cast<TH1*>(key->ReadObj());
+    if(hist==nullptr){
+      MSG_ERR("Error casting hist of type: "<<key->ClassName()<<" to TH1*, object named "<<key->GetName());
+    }
+    // const std::string name(hist->GetName());
+    TH1* base_hist = dynamic_cast<TH1*>(hist->Clone(name.c_str()));
+    for(int i =0; i < base_hist->GetNbinsX(); i++){
+      base_hist->SetBinContent(i,0);
+      base_hist->SetBinError(i,0);
+    }
+    TH2D* response_hist = setup_response_hist(base_hist);
+    response_hist = dynamic_cast<TH2D*>(make_response_hist(response_hist,tree,base_hist->GetName(),"SF*weight","_response"));
+    TH1* reco_hist = make_normal_hist(base_hist,tree,base_hist->GetName(),"SF*weight","_reco");
+    TH1* truth_hist = make_normal_hist(base_hist,tree,(std::string("truth_")+base_hist->GetName()),"SF*weight","_truth");
+    double N_sig = hist->Integral();
+    double N_MC = reco_hist->Integral();
+    MSG_DEBUG("NSig: "<<N_sig<<" N_MC: "<<N_MC);
+    reco_hist->Scale(N_sig/N_MC);
+    truth_hist->Scale(N_sig/N_MC);//n_truth?
+    if(print_validation_plots){
+      //TODO Polish these and put them in supplement
+      TCanvas debug("debug","debug",600,600);
+      TLegend* leg = init_legend();
+      leg->AddEntry(reco_hist,"MC Reco","l");
+      leg->AddEntry(truth_hist,"MC Truth","l");
+      leg->AddEntry(hist,"Reco","l");
+      reco_hist->SetLineColor(kBlue);
+      reco_hist->Draw("HIST");
+      truth_hist->SetLineColor(kRed);
+      truth_hist->Draw("HIST same");
+      hist->SetLineColor(kBlack);
+      hist->Draw("HIST same");
+      leg->Draw();
+      debug.SaveAs((name+"reco_truth_debug.pdf").c_str());
+      debug.Clear();
+      response_hist->Draw("COLZ");
+      debug.SaveAs((name+"response_debug.pdf").c_str());
+    }
+    hist->SetLineColor(kBlack);
+    hist->SetMarkerSize(0);
+    hist->SetMarkerStyle(kDot);
+    truth_hist->SetLineColor(kRed);
+    truth_hist->SetMarkerSize(0);
+    truth_hist->SetMarkerStyle(kDot);
+    TLegend* leg = init_legend();
+    leg->AddEntry(hist,"Reco","l");
+    leg->AddEntry(truth_hist,"MC Truth","l");
+    TH1D* unfolded=NULL;
+    RooUnfoldResponse response(NULL, NULL,response_hist,
+			       (name+"_unfolded").c_str(),hist->GetTitle());
+    for(int i=1; i <= num_iter; i++){
+      TCanvas canv("canv","unfolded",600,600);
+      TLatex decorator;
+      decorator.SetTextSize(0.07);
+      // if(name=="delta_r"){
+      // 	canv.SetLogy(true);
+      // }
+      RooUnfoldBayes unfold(&response,hist,i);
       MSG_DEBUG(unfold.Chi2(truth_hist,RooUnfold::kCovariance));
       unfolded = dynamic_cast<TH1D*>(unfold.Hreco(RooUnfold::kCovariance));
-      /*
-      for(size_t i=1; i < num_iter; i++){
-
-      }
-      */ 
-      TCanvas canv("canv","unfolded",600,600);
-      // TH1D* unfolded_bbb = dynamic_cast<TH1D*>(UnfoldBBB.Hreco(RooUnfold::kCovariance));
-      // unfolded_bbb->SetLineColor(kBlue);
-      // unfolded_bbb->Draw("e0");
       unfolded->SetLineColor(kBlue);
       unfolded->SetMarkerSize(0);
       unfolded->SetMarkerStyle(kDot);
-      unfolded->Draw("H same");
-
-      hist->SetLineColor(kBlack);
-      hist->SetMarkerSize(0);
-      hist->SetMarkerStyle(kDot);
+      unfolded->Draw("H");
       hist->Draw("H same");
-
-      truth_hist->SetLineColor(kRed);
-      truth_hist->SetMarkerSize(0);
-      truth_hist->SetMarkerStyle(kDot);
       truth_hist->Draw("HIST same");
-      TLegend* leg = init_legend();
-      // leg->AddEntry(unfolded_bbb,"Unfolded Bin-by-Bin","l");
-      leg->AddEntry(unfolded,"Iterative Bayes","l");
-      leg->AddEntry(hist,"Reco","l");
-      leg->AddEntry(truth_hist,"MC Truth","l");
+      if(i==1){
+	leg->AddEntry(unfolded,"Unfolded","l");
+      }
       leg->Draw();
-      canv.SaveAs((name+"_unfolded.pdf").c_str());
-      // break;
+      char label[100];
+      snprintf(label,100,"k=%d",i);
+      decorator.DrawLatexNDC(0.2,0.85,label);
+      char out_name[200];
+      snprintf(out_name,200,"%s_%02d_unfolded.pdf",name.c_str(),i);
+      canv.SaveAs(out_name);
+      
     }
+
   }
   reco_file->Close();
   
--- bin/unfolding-closure.cxx
+++ bin/unfolding-closure.cxx
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include "TMath.h"
+#include "TMatrixD.h"
+#include "TVectorD.h"
 #include "TH1D.h"
+#include "TH1.h"
+#include "TH2D.h"
 #include "TTree.h"
 
 #include "histo-meta-data.hh"
@@ -11,7 +16,26 @@
 void usage(const char* name){
   MSG("Usage: "<< name << "-l lumi -i input.root -o output.root");
 }
-
+TMatrixD make_response_matrix(TTree* tree, TH1* base_hist,const char* weight_expr){
+    TH2D* response_hist = setup_response_hist(base_hist);
+    response_hist->SetDirectory(NULL);
+    response_hist = dynamic_cast<TH2D*>(make_response_hist(response_hist,tree,base_hist->GetName(),weight_expr,"_response"));
+    for(int i =1; i <= response_hist->GetNbinsX(); i++){
+      TH1* hist = response_hist->ProjectionY("_py",i,i+1,"e");
+      hist->SetDirectory(NULL);
+      double norm_factor = hist->Integral();
+      MSG_DEBUG("Norm factor: "<<norm_factor);
+      if(norm_factor==0){
+	continue;
+      }
+      for(int j = 0; j < response_hist->GetNbinsY(); j++){
+	double bc=response_hist->GetBinContent(i,j);
+	response_hist->SetBinContent(i,j,bc/norm_factor);
+      }
+    }
+    return TMatrixD(response_hist->GetNbinsX()+2,response_hist->GetNbinsY()+2,
+		    response_hist->GetArray(),"D");
+}
 
 int main(const int argc, char* const argv[]){
   char* in_fname=nullptr;
@@ -49,8 +73,22 @@ int main(const int argc, char* const argv[]){
   for(std::map<std::string,TH1D*>::iterator it=HistBook.begin();
       it!=HistBook.end(); ++it){
     const std::string& name = it->first;
-    TH1* hist = make_normal_hist(it->second,tree,name.c_str(),"weight");
-    hist->SetName(name.c_str());
+    TMatrixD response_matrix = make_response_matrix(tree,it->second,"");
+    TH1D* hist = dynamic_cast<TH1D*>(make_normal_hist(it->second,tree,("truth_"+name).c_str(),""));
+    hist->SetName(("tmp_"+name).c_str());
+    hist->SetDirectory(NULL);
+    TH1D* truth_hist = dynamic_cast<TH1D*>(hist->Clone(("truth_"+name).c_str()));
+    truth_hist->Reset();
+    truth_hist->FillRandom(hist,171000);
+    TVectorD true_vec(truth_hist->GetNbinsX()+2,truth_hist->GetArray());
+    TVectorD rec_vec = response_matrix*true_vec;
+    hist->SetName(("truth_"+name).c_str());
+    TH1D* rec_hist = dynamic_cast<TH1D*>(hist->Clone(name.c_str()));
+    rec_hist->Reset();
+    for(int i =0; i < rec_hist->GetNbinsX(); i++){
+      rec_hist->SetBinContent(i,rec_vec[i]);
+      rec_hist->SetBinError(i,TMath::Sqrt(rec_vec[i]));
+    }
   }
   out_file.Write();
   out_file.Close();
